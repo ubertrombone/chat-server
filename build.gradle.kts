489b8487365147c25f4ca16bf4c959c18b1afd59
ubertrombone <josh_rose@alumni.baylor.edu>
2023-11-24 20:53:54 +0200

    Updates gradle files to use version catalog.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,7 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val exposed_version: String by project
-
 plugins {
-    kotlin("jvm") version "1.8.0"
-    id("io.ktor.plugin") version "2.2.2"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.0"
+    alias(deps.plugins.kotlin.jvm)
+    alias(deps.plugins.ktor.plugin)
+    alias(deps.plugins.kotlin.serialization)
 }
 
 group = "com.joshrose"
@@ -23,27 +18,26 @@ repositories {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
-    implementation("org.xerial:sqlite-jdbc:3.40.0.0")
-    implementation("commons-codec:commons-codec:1.15")
-    implementation("io.ktor:ktor-server-core-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-auth-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-host-common-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-status-pages-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-call-logging-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.2.3")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-websockets-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-netty-jvm:2.2.3")
-    implementation("io.ktor:ktor-server-request-validation:2.2.3")
-    implementation("io.ktor:ktor-client-auth-jvm:2.2.3")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.ktor:ktor-server-test-host-jvm:2.2.3")
-    testImplementation("io.ktor:ktor-server-tests-jvm:2.2.3")
-    testImplementation("io.ktor:ktor-client-content-negotiation:2.2.3")
-    testImplementation("io.ktor:ktor-server-test-host-jvm:2.2.3")
+    implementation(deps.logback.logbackClassic)
+    implementation(deps.exposed.core)
+    implementation(deps.exposed.dao)
+    implementation(deps.exposed.jdbc)
+    implementation(deps.exposed.javaTime)
+    implementation(deps.xerial.sqliteJdbc)
+    implementation(deps.codec.commons)
+    implementation(deps.ktor.serverCore)
+    implementation(deps.ktor.serverAuth)
+    implementation(deps.ktor.serverHostCommon)
+    implementation(deps.ktor.serverStatusPages)
+    implementation(deps.ktor.serverCallLogging)
+    implementation(deps.ktor.serverContentNegotiation)
+    implementation(deps.ktor.serializationKotlinxJson)
+    implementation(deps.ktor.serverWebsockets)
+    implementation(deps.ktor.serverNetty)
+    implementation(deps.ktor.serverRequestValidation)
+    implementation(deps.ktor.clientAuth)
+    testImplementation(deps.kotlin.testJunit)
+    testImplementation(deps.ktor.serverTestHost)
+    testImplementation(deps.ktor.serverTests)
+    testImplementation(deps.ktor.clientContentNegotiation)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
+@file:Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("deps") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+}
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "chat-server"
