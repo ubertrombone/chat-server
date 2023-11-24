a19eaf22b5d37133fd159fd5ccb1fe7d03adc0e8
ubertrombone <josh_rose@alumni.baylor.edu>
2023-11-24 21:24:12 +0200

    Prepares app to switch to JWT auth.

--- src/main/kotlin/com/joshrose/plugins/Security.kt
+++ src/main/kotlin/com/joshrose/plugins/Security.kt
@@ -1,10 +1,33 @@
 package com.joshrose.plugins
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import kotlinx.serialization.Serializable
 
 fun Application.configureSecurity() {
+    // Please read the jwt property from the config file if you are using EngineMain
+    val jwtAudience = environment.config.property("jwt.audience").getString()
+    val jwtDomain = environment.config.property("jwt.issuer").getString()
+    val jwtRealm = environment.config.property("jwt.realm").getString()
+    val jwtSecret = environment.config.property("jwt.secret").getString()
+    authentication {
+        jwt {
+            realm = jwtRealm
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(jwtSecret))
+                    .withAudience(jwtAudience)
+                    .withIssuer(jwtDomain)
+                    .build()
+            )
+            validate { credential ->
+                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+            }
+        }
+    }
 
     authentication {
         basic {
