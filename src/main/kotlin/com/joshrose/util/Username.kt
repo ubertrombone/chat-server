30980fda3d48c5577312255bb2b3eefd08b4bcd9
ubertrombone <josh_rose@alumni.baylor.edu>
2023-11-26 20:26:30 +0200

    Inlines username value to reduce need for unique data class and validation functions.

---
+++ src/main/kotlin/com/joshrose/util/Username.kt
@@ -0,0 +1,15 @@
+package com.joshrose.util
+
+import com.joshrose.Constants.REQUIREMENT_MAX
+import com.joshrose.Constants.USERNAME_TOO_LONG
+import com.joshrose.Constants.USERNAME_TOO_SHORT
+import kotlinx.serialization.Serializable
+
+@Serializable
+@JvmInline
+value class Username(val name: String) {
+    init {
+        require(name.isNotEmpty()) { USERNAME_TOO_SHORT }
+        require(name.length <= REQUIREMENT_MAX) { USERNAME_TOO_LONG }
+    }
+}
