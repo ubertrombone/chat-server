85e77abba0594c70156b7378bbf9da0dcece791e
ubertrombone <josh_rose@alumni.baylor.edu>
2023-11-28 20:07:57 +0200

    Removes TODO

--- src/main/kotlin/com/joshrose/requests/AccountRequest.kt
+++ src/main/kotlin/com/joshrose/requests/AccountRequest.kt
@@ -3,7 +3,6 @@ package com.joshrose.requests
 import com.joshrose.util.Username
 import kotlinx.serialization.Serializable
 
-// TODO: Make password and inline class too?
 @Serializable
 data class AccountRequest(
     val username: Username,
