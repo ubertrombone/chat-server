5be97a36ecac8ca90b9d5abc9651e4f40eef43dd
ubertrombone <josh_rose@alumni.baylor.edu>
2023-12-17 22:23:22 +0200

    Makes validation check archive for username existence.

--- src/main/kotlin/com/joshrose/validations/ValidateUsername.kt
+++ src/main/kotlin/com/joshrose/validations/ValidateUsername.kt
@@ -1,10 +1,7 @@
 package com.joshrose.validations
 
-import com.joshrose.Constants.INVALID_CHARS_USERNAME
-import com.joshrose.Constants.REQUIREMENT_MAX
 import com.joshrose.Constants.USERNAME_EXISTS
-import com.joshrose.Constants.USERNAME_TOO_LONG
-import com.joshrose.Constants.USERNAME_TOO_SHORT
+import com.joshrose.plugins.archiveDao
 import com.joshrose.plugins.dao
 import com.joshrose.requests.AccountRequest
 import io.ktor.server.plugins.requestvalidation.*
@@ -13,9 +10,7 @@ import io.ktor.server.plugins.requestvalidation.ValidationResult.*
 fun RequestValidationConfig.validateUsername() {
     validate<AccountRequest> { account ->
         when {
-            account.username.name.isBlank() -> Invalid(USERNAME_TOO_SHORT)
-            account.username.name.length > REQUIREMENT_MAX -> Invalid(USERNAME_TOO_LONG)
-            account.username.name.any { !it.isLetterOrDigit() } -> Invalid(INVALID_CHARS_USERNAME)
+            archiveDao.getArchivedUser(account.username) != null -> Invalid(USERNAME_EXISTS)
             dao.usernameExists(account.username) -> Invalid(USERNAME_EXISTS)
             else -> Valid
         }
