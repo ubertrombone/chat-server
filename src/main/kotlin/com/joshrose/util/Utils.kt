56c3ffc1a7266c9b5fcdb157b89f6abb1c93fb0b
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-01 20:25:10 +0200

    Fixes bad login in toUsernameOrNull; adds receiveOrNull

--- src/main/kotlin/com/joshrose/util/Utils.kt
+++ src/main/kotlin/com/joshrose/util/Utils.kt
@@ -1,4 +1,11 @@
 package com.joshrose.util
 
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+
 fun String.toUsername() = Username(name = this)
-fun String?.toUsernameOrNull() = this?.let { Username(this) }
+fun String.toUsernameOrNull() =
+    try { Username(this) } catch (e: IllegalArgumentException) { null }
+
+suspend inline fun <reified T: Any> ApplicationCall.receiveOrNull(): T? =
+    try { receive<T>() } catch (e: ContentTransformationException) { null }
