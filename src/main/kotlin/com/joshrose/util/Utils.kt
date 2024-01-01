71759d9f9fb1ba32c5116e1f01e4e4656cf62428
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-01 21:04:57 +0200

    Refactors util functions

--- src/main/kotlin/com/joshrose/util/Utils.kt
+++ src/main/kotlin/com/joshrose/util/Utils.kt
@@ -4,8 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 
 fun String.toUsername() = Username(name = this)
-fun String.toUsernameOrNull() =
-    try { Username(this) } catch (e: IllegalArgumentException) { null }
+fun String.toUsernameOrNull() = runCatching { toUsername() }.getOrNull()
 
 suspend inline fun <reified T: Any> ApplicationCall.receiveOrNull(): T? =
-    try { receive<T>() } catch (e: ContentTransformationException) { null }
+    runCatching { receive<T>() }.getOrNull()
