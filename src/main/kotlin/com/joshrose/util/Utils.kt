6e7e956ed2e19b6e793dc503e1bbb94e354bca43
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-03 20:35:35 +0200

    Fixes issue with validation getting ignored.

--- src/main/kotlin/com/joshrose/util/Utils.kt
+++ src/main/kotlin/com/joshrose/util/Utils.kt
@@ -1,10 +1,36 @@
 package com.joshrose.util
 
+import com.joshrose.Constants.GROUP_NAME_EXISTS
+import com.joshrose.Constants.GROUP_NAME_INVALID_CHARS
+import com.joshrose.Constants.GROUP_NAME_TOO_LONG
+import com.joshrose.Constants.GROUP_NAME_TOO_SHORT
+import com.joshrose.Constants.INCORRECT_CREDS
+import com.joshrose.Constants.STATUS_TOO_LONG
+import com.joshrose.Constants.UNKNOWN_ERROR
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Conflict
+import io.ktor.http.HttpStatusCode.Companion.UnprocessableEntity
 import io.ktor.server.application.*
+import io.ktor.server.plugins.requestvalidation.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
 
 fun String.toUsername() = Username(name = this)
 fun String.toUsernameOrNull() = runCatching { toUsername() }.getOrNull()
 
 suspend inline fun <reified T: Any> ApplicationCall.receiveOrNull(): T? =
-    runCatching { receive<T>() }.getOrNull()
+    runCatching { receive<T>() }.getOrElse {
+        if (it is RequestValidationException) {
+            with(it.reasons) {
+                when {
+                    contains(GROUP_NAME_EXISTS) -> respond(Conflict, joinToString())
+                    contains(STATUS_TOO_LONG) -> respond(UnprocessableEntity, joinToString())
+                    contains(GROUP_NAME_TOO_SHORT) -> respond(UnprocessableEntity, joinToString())
+                    contains(GROUP_NAME_TOO_LONG) -> respond(UnprocessableEntity, joinToString())
+                    contains(GROUP_NAME_INVALID_CHARS) -> respond(UnprocessableEntity, joinToString())
+                    contains(INCORRECT_CREDS) -> respond(BadRequest, joinToString())
+                }
+            }
+        } else respond(BadRequest, UNKNOWN_ERROR)
+        null
+    }
