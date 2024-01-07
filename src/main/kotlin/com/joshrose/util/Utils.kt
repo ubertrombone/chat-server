7050139ca7e4149c1c351a8766e586e3e159f3df
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-07 14:36:56 +0200

    Removes checks for values already check on client side.

--- src/main/kotlin/com/joshrose/util/Utils.kt
+++ src/main/kotlin/com/joshrose/util/Utils.kt
@@ -5,16 +5,9 @@ import com.joshrose.Constants.GROUP_NAME_INVALID_CHARS
 import com.joshrose.Constants.GROUP_NAME_TOO_LONG
 import com.joshrose.Constants.GROUP_NAME_TOO_SHORT
 import com.joshrose.Constants.INCORRECT_CREDS
-import com.joshrose.Constants.INVALID_CHARS_PASSWORD
-import com.joshrose.Constants.INVALID_CHARS_USERNAME
-import com.joshrose.Constants.PASSWORD_LONG
-import com.joshrose.Constants.PASSWORD_REQUIRED_CHARS
-import com.joshrose.Constants.PASSWORD_SHORT
 import com.joshrose.Constants.UNKNOWN_ERROR
 import com.joshrose.Constants.USERNAME_DOESNT_EXIST
 import com.joshrose.Constants.USERNAME_EXISTS
-import com.joshrose.Constants.USERNAME_TOO_LONG
-import com.joshrose.Constants.USERNAME_TOO_SHORT
 import io.ktor.http.HttpStatusCode.Companion.BadRequest
 import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.UnprocessableEntity
@@ -26,6 +19,7 @@ import io.ktor.server.response.*
 fun String.toUsername() = Username(name = this)
 fun String.toUsernameOrNull() = runCatching { toUsername() }.getOrNull()
 
+// TODO: Get rid of GroupChat name checks too eventually
 @Suppress("DuplicatedCode")
 suspend inline fun <reified T: Any> ApplicationCall.receiveOrNull(): T? =
     runCatching { receive<T>() }.getOrElse { throwable ->
@@ -33,11 +27,8 @@ suspend inline fun <reified T: Any> ApplicationCall.receiveOrNull(): T? =
             with(throwable.reasons) {
                 when {
                     any { it == USERNAME_EXISTS || it == GROUP_NAME_EXISTS } -> respond(Conflict, joinToString())
-                    any {
-                        it == PASSWORD_SHORT || it == USERNAME_TOO_LONG || it == INVALID_CHARS_USERNAME ||
-                            it == PASSWORD_LONG || it == PASSWORD_REQUIRED_CHARS || it == INVALID_CHARS_PASSWORD ||
-                            it == USERNAME_DOESNT_EXIST || it == USERNAME_TOO_SHORT ||
-                            it == GROUP_NAME_TOO_SHORT || it == GROUP_NAME_TOO_LONG || it == GROUP_NAME_INVALID_CHARS
+                    any { it == USERNAME_DOESNT_EXIST || it == GROUP_NAME_TOO_SHORT ||
+                            it == GROUP_NAME_TOO_LONG || it == GROUP_NAME_INVALID_CHARS
                     } -> respond(UnprocessableEntity, joinToString())
                     contains(INCORRECT_CREDS) -> respond(BadRequest, joinToString())
                 }
