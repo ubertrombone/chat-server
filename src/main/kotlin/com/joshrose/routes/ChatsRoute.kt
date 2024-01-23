5d4dcea5d39f93f9921596706bf2ab35be71f7c9
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-23 20:33:53 +0200

    Removes POST /delete_chat endpoint

--- src/main/kotlin/com/joshrose/routes/ChatsRoute.kt
+++ src/main/kotlin/com/joshrose/routes/ChatsRoute.kt
@@ -1,7 +1,6 @@
 package com.joshrose.routes
 
 import com.joshrose.plugins.cacheDao
-import com.joshrose.plugins.chatDao
 import com.joshrose.util.receiveOrNull
 import io.ktor.http.HttpStatusCode.Companion.BadRequest
 import io.ktor.http.HttpStatusCode.Companion.OK
@@ -13,13 +12,6 @@ import io.ktor.server.routing.*
 fun Route.chatsRoute() {
     route("/chat_actions") {
         authenticate {
-            post("/delete_chat") {
-                val chat = call.receiveOrNull<Int>() ?: return@post
-
-                if (chatDao.deleteChat(chat)) call.respond(OK, "Chat successfully deleted!")
-                else call.respond(BadRequest, "Error deleting chat!")
-            }
-
             post("/delete_message") {
                 val message = call.receiveOrNull<Int>() ?: return@post
 
