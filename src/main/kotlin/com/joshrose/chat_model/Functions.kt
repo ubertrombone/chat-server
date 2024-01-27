28c748d0515d9463f643a2828fda0f02f9852637
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-27 23:00:29 +0200

    Tries to add error handling as a Function, but this isn't working.

--- src/main/kotlin/com/joshrose/chat_model/Functions.kt
+++ src/main/kotlin/com/joshrose/chat_model/Functions.kt
@@ -1,8 +1,13 @@
 package com.joshrose.chat_model
 
-enum class Functions {
-    GROUP,
-    INDIVIDUAL,
-    LEAVE,
-    JOIN
+import com.joshrose.util.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface Functions {
+    @Serializable(with = GroupAsStringSerializer::class) data class Group(val message: String? = null) : Functions
+    @Serializable(with = IndividualAsStringSerializer::class) data class Individual(val message: String? = null) : Functions
+    @Serializable(with = LeaveAsStringSerializer::class) data class Leave(val message: String? = null) : Functions
+    @Serializable(with = JoinAsStringSerializer::class) data class Join(val message: String? = null) : Functions
+    @Serializable(with = ErrorAsStringSerializer::class) data class Error(val message: String) : Functions
 }
