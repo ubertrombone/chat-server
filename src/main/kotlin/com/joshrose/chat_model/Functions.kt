5e54aafd013717b7680655c8aa59eedce5646e4d
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-28 14:24:00 +0200

    Removes unused code

--- src/main/kotlin/com/joshrose/chat_model/Functions.kt
+++ src/main/kotlin/com/joshrose/chat_model/Functions.kt
@@ -7,13 +7,3 @@ enum class Functions {
     LEAVE,
     ERROR
 }
-
-// TODO: Return this to enum class and add just ERROR
-//@Serializable
-//sealed interface Functions {
-//    @Serializable(with = GroupAsStringSerializer::class) data class Group(val message: String? = null) : Functions
-//    @Serializable(with = IndividualAsStringSerializer::class) data class Individual(val message: String? = null) : Functions
-//    @Serializable(with = LeaveAsStringSerializer::class) data class Leave(val message: String? = null) : Functions
-//    @Serializable(with = JoinAsStringSerializer::class) data class Join(val message: String? = null) : Functions
-//    @Serializable(with = ErrorAsStringSerializer::class) data class Error(val message: String) : Functions
-//}
