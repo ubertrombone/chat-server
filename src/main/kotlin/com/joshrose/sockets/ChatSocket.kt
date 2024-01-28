579f50713ab21026a5dbec76bdb57086c4182d4b
ubertrombone <josh_rose@alumni.baylor.edu>
2024-01-28 13:15:21 +0200

    Updates to handle error responses returned to sender; updates receiver Type for establishConnection and handleIncomingFrames to DefaultWebSocketServerSession; adds log call to log response.

--- src/main/kotlin/com/joshrose/sockets/ChatSocket.kt
+++ src/main/kotlin/com/joshrose/sockets/ChatSocket.kt
@@ -4,6 +4,7 @@ import com.joshrose.Connection
 import com.joshrose.chat_model.ChatMessage
 import com.joshrose.chat_model.Functions.*
 import com.joshrose.plugins.groupChatDao
+import com.joshrose.responses.SimpleResponse
 import com.joshrose.util.toUsername
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
@@ -11,6 +12,7 @@ import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.channels.consumeEach
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 const val CHAT_ROUTE = "/chat"
@@ -32,19 +34,23 @@ fun Route.chatSocket() {
     }
 }
 
-suspend fun DefaultWebSocketSession.establishConnection(principal: JWTPrincipal, server: ChatServer): Connection {
+suspend fun DefaultWebSocketServerSession.establishConnection(principal: JWTPrincipal, server: ChatServer): Connection {
     val username = principal.payload.getClaim("username").asString().toUsername()
     val connection = Connection(session = this, name = username)
     server.addConnection(connection)
     return connection
 }
 
-suspend fun DefaultWebSocketSession.handleIncomingFrames(server: ChatServer, connection: Connection) =
+suspend fun DefaultWebSocketServerSession.handleIncomingFrames(server: ChatServer, connection: Connection) =
     incoming.consumeEach { frame ->
         if (frame is Frame.Text) {
             val message = Json.decodeFromString<ChatMessage>(frame.readText())
             val response = delegateMessageProcessing(message, server, connection)
-            if (!response.successful) connection.session.send("An error occurred: ${response.message}")
+            call.application.environment.log.info(Json.encodeToString(response))
+            if (!response.successful) {
+                val err = message.copy(function = ERROR, error = response.message)
+                connection.session.send(Json.encodeToString<ChatMessage>(err))
+            }
         }
     }
 
@@ -60,4 +66,5 @@ suspend fun delegateMessageProcessing(message: ChatMessage, server: ChatServer, 
             val group = groupChatDao.groupChat(message.recipientOrGroup)
             server.joinGroup(group, connection)
         }
+        ERROR -> { SimpleResponse(true, "") }
     }
