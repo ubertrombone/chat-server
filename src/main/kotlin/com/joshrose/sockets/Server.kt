43dab99ec462d56ca0adbefd16edef476ad71bc0
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-13 20:57:58 +0200

    Adds new functions.

--- src/main/kotlin/com/joshrose/sockets/Server.kt
+++ src/main/kotlin/com/joshrose/sockets/Server.kt
@@ -3,9 +3,12 @@ package com.joshrose.sockets
 import com.joshrose.Connection
 import com.joshrose.chat_model.FriendChatMessage
 import com.joshrose.responses.SimpleResponse
+import com.joshrose.util.Username
 
 interface Server {
+    suspend fun establishConnection(username: Username): Connection
     suspend fun addConnection(connection: Connection): Boolean
     suspend fun removeConnection(connection: Connection): Boolean
     suspend fun sendMessage(message: FriendChatMessage): SimpleResponse
+    suspend fun handleIncomingFrames(connection: Connection)
 }
