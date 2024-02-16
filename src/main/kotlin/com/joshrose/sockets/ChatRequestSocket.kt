170d370c92df80b2db6fe5b41e91fc3eda7977aa
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-16 20:24:42 +0200

    Moves server out of websocket scope. I think having it in that scope would have caused some unexpected behavior.

--- src/main/kotlin/com/joshrose/sockets/ChatRequestSocket.kt
+++ src/main/kotlin/com/joshrose/sockets/ChatRequestSocket.kt
@@ -8,14 +8,15 @@ import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 
 fun Route.chatRequest() {
+    val server = ChatRequestServer()
+
     authenticate {
         webSocket(CHAT_ROUTE) {
-            val server = ChatRequestServer(this)
             val userPrincipal = call.principal<JWTPrincipal>()!!.payload.getClaim("username").asString().toUsername()
-            val thisConnection = server.establishConnection(userPrincipal)
+            val thisConnection = server.establishConnection(userPrincipal, this)
 
             try {
-                server.handleIncomingFrames(thisConnection)
+                server.handleIncomingFrames(thisConnection, this)
             } finally {
                 server.removeConnection(thisConnection)
             }
