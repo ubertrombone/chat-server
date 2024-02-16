8b5b0d82f4b0190746e7c9ea4da2444818821112
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-16 20:35:08 +0200

    Adds chatId and session parameters to functions

--- src/main/kotlin/com/joshrose/sockets/Server.kt
+++ src/main/kotlin/com/joshrose/sockets/Server.kt
@@ -3,11 +3,12 @@ package com.joshrose.sockets
 import com.joshrose.Connection
 import com.joshrose.responses.SimpleResponse
 import com.joshrose.util.Username
+import io.ktor.server.websocket.*
 
 interface Server {
-    suspend fun establishConnection(username: Username): Connection
+    suspend fun establishConnection(username: Username, chatId: Int, session: DefaultWebSocketServerSession): Connection
     suspend fun addConnection(connection: Connection): Boolean
     suspend fun removeConnection(connection: Connection): Boolean
     suspend fun <T> sendMessage(message: T): SimpleResponse
-    suspend fun handleIncomingFrames(connection: Connection)
+    suspend fun handleIncomingFrames(connection: Connection, session: DefaultWebSocketServerSession)
 }
