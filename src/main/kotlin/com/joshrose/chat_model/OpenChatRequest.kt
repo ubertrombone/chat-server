60edfd7a68dd0f09a5db52d8ecc0f2b46a808780
ubertrombone <josh_rose@alumni.baylor.edu>
2024-02-18 13:05:17 +0200

    Updates properties to be of type Username

--- src/main/kotlin/com/joshrose/chat_model/OpenChatRequest.kt
+++ src/main/kotlin/com/joshrose/chat_model/OpenChatRequest.kt
@@ -1,9 +1,10 @@
 package com.joshrose.chat_model
 
+import com.joshrose.util.Username
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class OpenChatRequest(
-    val sender: Int,
-    val recipient: Int
+    val sender: Username,
+    val recipient: Username
 )
